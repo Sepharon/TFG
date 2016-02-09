2383b472a253f056dcadb81d557ccd52abf3ef0c
Silvia <quercusroses@gmail.com>
2016-02-09 10:08:19 +0100

    Simple app

--- NeedMoreCookies/app/src/main/java/sersilinc/needmorecookies/MainActivity.java
+++ NeedMoreCookies/app/src/main/java/sersilinc/needmorecookies/MainActivity.java
@@ -1,17 +1,109 @@
 package sersilinc.needmorecookies;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+
+import android.os.CountDownTimer;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // call functions from service usuing data.function_name()
+    //Weather_Data data;
+    Messenger mService = null;
+    boolean is_bound = false;
+
+
+    TextView Data1;
+    TextView Data2;
+
+
+    String data1="";
+    String data2="";
+
+
+    Button b;
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Unbind from the service
+        if (is_bound) {
+            unbindService(mConnection);
+            is_bound = false;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, RequestGet.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+
+        IntentFilter filter = new IntentFilter("miss_temps");
+        this.registerReceiver(new MyReceiver(), filter);
+
+        Data1 = (TextView)findViewById(R.id.data1);
+        Data2 = (TextView)findViewById(R.id.data2);
+
+
+        // Sending data to Service
+        Log.v("Activity:", "Sending message");
+        b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (is_bound) {
+                    //if (!is_bound) return;
+                    // Create and send a message to the service, using a supported 'what' value
+                    Log.v("Activity:", "Getting ready");
+                    Message msg = Message.obtain(null, RequestGet.MSG_GET_DATA);
+                    Bundle bundle = new Bundle();
+
+
+                    msg.setData(bundle);
+                    try {
+                        mService.send(msg);
+                        Log.v("Activity:", "Message sent");
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Done sending data
+                }
+            }
+        });
+
+
+        new CountDownTimer(300000, 1000) { //5min
+            public void onTick(long millisUntilFinished) {}
+            public void onFinish() {
+                //Write function to be called
+                reload();
+                start();
+            }
+        }.start();
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -28,10 +120,67 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+
         if (id == R.id.action_settings) {
+            //openAbout();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    /*
+    public void openAbout() {
+        Intent intent = new Intent(MainActivity.this, About.class);
+        startActivity(intent);
+    }
+*/
+
+    // Binding function
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            Log.v("Main Activity:", "Binding service");
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            //Weather_Data.LocalBinder binder = (Weather_Data.LocalBinder) service;
+            //data = binder.getService();
+            mService = new Messenger(service);
+            is_bound = true;
+        }
+
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mService = null;
+            is_bound = false;
+        }
+    };
+
+    public class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            data1 = intent.getStringExtra("data1");
+            data2 = intent.getStringExtra("data2");
+
+            Data1.setText(data1);
+            Data2.setText(data2);
+
+            Log.v("Activity One result", data1);
+        }
+    }
+
+    public void reload(){
+        Data1.setText(data1);
+        Data2.setText(data2);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+
 }
---
+++ NeedMoreCookies/app/src/main/java/sersilinc/needmorecookies/RequestGet.java
@@ -0,0 +1,198 @@
+package sersilinc.needmorecookies;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Binder;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.StrictMode;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+public class RequestGet extends Service {
+    JSONObject DATA = null;
+    JSONObject DATA2 = null;
+    JSONObject mainData = null;
+    JSONArray data_array = null;
+
+    static final int MSG_GET_DATA = 1;
+    static final String url = "http://www.tfgcentrethailam.com";
+    static final String request = "/data/2.5/weather?";
+
+    private final IBinder mBinder = new LocalBinder();
+    private Messenger msg = new Messenger(new IncomingHandler());
+
+    String result_city="";
+    String result_country="";
+    String temp_units="";
+
+    boolean first=true;
+
+    public class LocalBinder extends Binder {
+        RequestGet getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return RequestGet.this;
+        }
+    }
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.v("Service:", "Binding");
+        return msg.getBinder();
+    }
+
+    @Override
+    public void onCreate (){
+        super.onCreate();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+        // f(total_countdown_time,tick_time)
+        // 5 minutes, ticks every second
+        if (!is_network_available()) {
+            Toast.makeText(getBaseContext(), "No network available", Toast.LENGTH_LONG).show();
+        }
+        else {
+            if (first) {
+                first=false;
+                Log.v("Service: ", "Started countdown");
+                new CountDownTimer(18000, 1000) {
+                    public void onTick(long millisUntilFinished) {
+                    }
+
+                    public void onFinish() {
+                        //Write function to be called
+                        get_weather_data();
+                        start();
+                    }
+                }.start();
+            } else {
+                Log.v("Service: ", "Started countdown");
+                new CountDownTimer(300000, 1) { //5min
+                    public void onTick(long millisUntilFinished) {
+                    }
+
+                    public void onFinish() {
+                        //Write function to be called
+                        get_weather_data();
+                        start();
+                    }
+                }.start();
+            }
+        }
+    }
+
+    public void get_weather_data(){
+        int responseCode;
+        Log.v("temp_unit", ""+temp_units);
+
+        String full_url = url+request+"q="+result_city+","+result_country+"&units="+temp_units+"&APPID=3e16d61afeec2d2b55c477eaf523cb20";
+        Log.v("Service:", "full url = " + full_url);
+
+        try{
+            Log.v("Service:" , "Creating connection");
+            URL link_url = new URL(full_url);
+            // Opening connection
+            URLConnection connection = link_url.openConnection();
+            Log.v("Service:" , "Trying connection");
+            HttpURLConnection httpConnection = (HttpURLConnection) connection;
+            // Checking if connection exists
+            responseCode = httpConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK){
+                httpConnection.connect();
+                Log.v("Service:", "Connection ok, getting input");
+                InputStream stream = httpConnection.getInputStream();
+                Log.v("Service:" , "Reading stream");
+                // Read data
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+                String json = reader.readLine();
+                Log.v("Service: ", json);
+                // Put the data in a JSONObject
+                DATA2 = new JSONObject(json);
+                mainData = DATA2.getJSONObject("main");
+                DATA = DATA2.getJSONObject("wind");
+
+                Log.v("Services:", mainData.getString("temp"));
+                Log.v("Service: ", mainData.getString("temp_min"));
+                Log.v("Services:", mainData.getString("temp_max"));
+                Log.v("Services:", mainData.getString("pressure"));
+                Log.v("Services:", mainData.getString("humidity"));
+
+                data_array = DATA.getJSONArray("weather");
+                Log.v("Service: ", data_array.getJSONObject(0).getString("main"));
+
+                Intent broadcast = new Intent();
+                broadcast.setAction("miss_temps");
+                broadcast.putExtra("temp", mainData.getString("temp"));
+                broadcast.putExtra("temp_min", mainData.getString("temp_min"));
+                broadcast.putExtra("temp_max", mainData.getString("temp_max"));
+                broadcast.putExtra("pressure", mainData.getString("pressure"));
+                broadcast.putExtra("humidity", mainData.getString("humidity"));
+                broadcast.putExtra("weather", data_array.getJSONObject(0).getString("main"));
+                broadcast.putExtra("units", temp_units);
+                sendBroadcast(broadcast);
+
+
+            }
+            else {
+                Log.v("Service: ", "city does not exist");
+                Toast.makeText(getBaseContext(),"The city does not exist",Toast.LENGTH_LONG).show();
+            }
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public boolean is_network_available(){
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            // Here write code for messages from activity (eg: city to get data from,
+            // units desired celius or farenheid)
+            switch (msg.what) {
+                case MSG_GET_DATA:
+
+                    Log.v("Service:", "Got data");
+                    result_city = msg.getData().getString("city");
+                    result_country = msg.getData().getString("country_code");
+                    temp_units = msg.getData().getString("unit");
+                    Toast.makeText(getApplicationContext(), "Requesting weather's data", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+}
